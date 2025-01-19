50e2cd3afc3ca3ae5bba45d5c311987bb5192448
TheAlup <thealph@hotmail.com>
2025-01-19 18:36:03 +0100

    Updated package-lock.json and script.mjs

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import HTTP_CODES from './utils/httpCodes.mjs';
 
 const server = express();
@@ -7,12 +7,24 @@ const port = (process.env.PORT || 8000);
 server.set('port', port);
 server.use(express.static('public'));
 
+// Route for "/"
 function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
 }
-
 server.get("/", getRoot);
 
+// Route for "/tmp/poem"
+function getPoem(req, res, next) {
+    res.status(HTTP_CODES.SUCCESS.OK).send(`
+        Roses are red,<br>
+        Violets are too,<br>
+        I tried to write code,<br>
+        But it just wouldn't do.
+    `).end();
+}
+server.get("/tmp/poem", getPoem);
+
+// Start the server
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
